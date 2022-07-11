c5823fd3b3e14f2689e2990507bc54abd11d0476
Oleg Sazonov <sazonov1994@gmail.com>
2022-07-11 19:36:46 +0300

    test

--- src/FiguresProject/Main.java
+++ src/FiguresProject/Main.java
@@ -4,6 +4,7 @@ import FiguresProject.Controllers.ControllerMenu;
 
 public class Main {
     public static void main(String[] args) {
+        //main
         ControllerMenu controllerMenu = new ControllerMenu();
         controllerMenu.mainMenu();
     }
